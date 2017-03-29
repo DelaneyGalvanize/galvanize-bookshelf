d409b9fc967d3eac4d6c8db8bd9847818594d2f7
Delaney Patterson <delaney.patterson001@gmail.com>
2017-03-28 20:12:24 -0600

    first migrations added (books)

---
+++ migrations/20170327152055_books.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('books', table => {
+    // table.serial primary key('id');
+    table.increments('id').primary();
+    table.string('title').notNullable().defaultTo('');
+    table.string('author').notNullable().defaultTo('');
+    table.string('genre').notNullable().defaultTo('');
+    table.text('description').notNullable().defaultTo('');
+    table.text('cover_url').notNullable().defaultTo('');
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('books');
+};
