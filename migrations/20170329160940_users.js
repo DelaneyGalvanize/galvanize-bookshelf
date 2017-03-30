548a6e40916872928b20082c181fe468d4c97ae1
Delaney Patterson <delaney.patterson001@gmail.com>
2017-03-29 21:02:34 -0600

    Add files via upload

---
+++ migrations/20170329160940_users.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name').notNullable().defaultTo('');
+    table.string('last_name').notNullable().defaultTo('');
+    table.string('email').notNullable().unique();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
