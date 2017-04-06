981457f7b062dba0d1e593384e4709a54a6d60c2
Delaney Patterson <delaney.patterson001@gmail.com>
2017-04-06 12:17:56 -0600

    Add files via upload

--- routes/token.js
+++ routes/token.js
@@ -8,47 +8,40 @@ const knex = require('../knex')
 const bcrypt = require('bcrypt')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, data) => {
-        if (err) {
-            res.status(200).send(false);
-        } else {
-            res.status(200).send(true);
-        }
-    });
+router.get('/token', (req, res, next) => {
+  if (!req.cookies.token) {
+    res.status(200).send(false)
+  } else {
+    res.status(200).send(true)
+  }
 })
 
-router.post('/', (req, res, next) => {
-    let email = req.body.email
-    let password = req.body.password
-    knex('users')
-        .where('email', email)
-        .then((userData) => {
-            if (userData.length > 0) {
-                bcrypt.compare(password, userData[0].hashed_password, (err, result) => {
-                    if (result) {
-                        let token = jwt.sign({
-                            email: userData[0].email,
-                            password: userData[0].hashed_password
-                        }, 'secret');
-                        res.cookie('token', token, {
-                            httpOnly: true
-                        })
-                        delete userData[0].hashed_password
-                        res.send(humps.camelizeKeys(userData[0]))
-                    } else {
-                        next(boom.create(400, 'Bad email or password'))
-                    }
-                })
-            } else {
-                next(boom.create(400, 'Bad email or password'))
-            }
+router.post('/token', (req, res, next) => {
+  let email = req.body.email
+  let password = req.body.password
+  knex('users')
+    .where('email', email)
+    .then((data) => {
+      if (data.length > 0) {
+        bcrypt.compare(password, data[0].hashed_password, (err, result) => {
+          if (result) {
+            let token = jwt.sign({ email: data[0].email, password: data[0].hashed_password}, "shhh");
+            res.cookie('token', token, { httpOnly:true })
+            delete data[0].hashed_password
+            res.send(humps.camelizeKeys(data[0]))
+          } else {
+            next(boom.create(400, 'Bad email or password'))
+          }
         })
+      } else {
+        next(boom.create(400, 'Bad email or password'))
+      }
+    })
 })
 
-router.delete('/', (req, res, session) => {
-    res.clearCookie('token')
-    res.send(true)
+router.delete('/token', (req, res, session) => {
+  res.clearCookie('token')
+  res.send(true)
 })
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,39 @@
-
 'use strict';
 
 const express = require('express');
-const knex = require('../knex');
-const bcrypt = require('bcrypt-as-promised');
+const knex = require('../knex')
 const humps = require('humps')
+const bcrypt = require('bcrypt')
+const boom = require('boom')
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
-  bcrypt.hash(req.body.password, 8)
-  .then((hashed_password) => {
-    return knex('users')
-    .insert({
-      first_name: req.body.firstName,
-      last_name: req.body.lastName,
-      email: req.body.email,
-      hashed_password: hashed_password
+router.post('/users', (req, res, next) => {
+  if (!req.body.email || req.body.email.trim() === '') {
+    return next(boom.create(400, 'Email must not be blank'))
+  } else if (!req.body.password || req.body.password.trim() === '') {
+    return next(boom.create(400, 'Password must be at least 8 characters long'))
+  }
+
+  knex('users')
+    .where('email', req.body.email)
+    .then((exists) => {
+      if (exists.length > 0) {
+        return next(boom.create(400, 'Email already exists'))
+      } else {
+        knex('users')
+          .returning(['id', 'first_name', 'last_name', 'email'])
+          .insert({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            hashed_password: bcrypt.hashSync(req.body.password, 10)
+          }).then((user) => {
+            console.log(user[0]);
+            res.send(humps.camelizeKeys(user[0]))
+          })
+      }
     })
-    .returning(['id', 'first_name', 'last_name', 'email']);
-
-  })
-
-  .then(userData =>{
-    delete userData[0].hashed_password;
-    res.send(humps.camelizeKeys(userData[0]));
-  })
 })
+
 
 module.exports = router;
