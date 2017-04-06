b55ee43e54c9277cf5d1a5c0e5c4da9339256ab6
Delaney Patterson <delaney.patterson001@gmail.com>
2017-04-05 19:14:09 -0600

    Add files via upload
    bonuses added!

--- routes/books.js
+++ routes/books.js
@@ -1,108 +1,108 @@
-
 'use strict';
 
 const express = require('express');
 const knex = require('../knex')
 const humps = require('humps')
-const knex = require('../knex')
-const humps = require('humps')
 const router = express.Router();
+const boom = require('boom')
 
 
 router.get('/books', (req, res, next) => {
-  knex('books')
-  .orderBy('title', 'asc')
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData));
-  });
+    knex('books')
+        .orderBy('title', 'asc')
+        .then(booksData => {
+            res.send(humps.camelizeKeys(booksData));
+        });
 });
 
 router.get('/books/:id', (req, res, next) => {
-  let id = req.params.id;
-  knex('books')
-  .where('id', id)
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData[0]));
-  });
+    let id = req.params.id;
+    if (isNaN(id)) {
+        return next(boom.create(404, 'Not Found'));
+    }
+    knex('books')
+        .where('id', id)
+        .then(booksData => {
+            if (booksData[0]) {
+                res.send(humps.camelizeKeys(booksData[0]));
+            } else {
+                return next(boom.create(404, 'Not Found'));
+            }
+        });
 });
 
+
 router.post('/books', (req, res, next) => {
-  knex('books')
-  .insert({
-    title: req.body.title,
-    author: req.body.author,
-    genre: req.body.genre,
-    description: req.body.description,
-    cover_url: req.body.coverUrl
-  }, '*')
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData[0]));
-  });
+    if (!req.body.title) {
+        return next(boom.create(400, 'Title must not be blank'));
+    }
+    if (!req.body.author) {
+        return next(boom.create(400, 'Author must not be blank'));
+    }
+    if (!req.body.genre) {
+        return next(boom.create(400, 'Genre must not be blank'));
+    }
+    if (!req.body.description) {
+        return next(boom.create(400, 'Description must not be blank'));
+    }
+    if (!req.body.coverUrl) {
+        return next(boom.create(400, 'Cover URL must not be blank'));
+    }
+    knex('books')
+        .returning('*')
+        .insert({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+            description: req.body.description,
+            cover_url: req.body.coverUrl
+        })
+        .then(booksData => {
+            res.send(humps.camelizeKeys(booksData[0]));
+        });
 });
 
 router.patch('/books/:id', (req, res, next) => {
-  let id = req.params.id;
-  knex('books')
-  .where('id', id)
-  .update(humps.decamelizeKeys(req.body))
-  .returning('*')
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData[0]));
-  });
+    let id = req.params.id
+    if (isNaN(id)) {
+        return next(boom.create(404, 'Not Found'));
+    }
+    knex('books')
+        .where('id', id)
+        .then((booksData) => {
+            if (booksData[0]) {
+                knex('books')
+                    .where('id', id)
+                    .update(humps.decamelizeKeys(req.body))
+                    .returning('*')
+                    .then(booksData => {
+                        res.send(humps.camelizeKeys(booksData[0]));
+                    })
+            } else {
+                return next(boom.create(404, 'Not Found'));
+            }
+
+        });
 });
 
 
-router.get('/books', (req, res, next) => {
-  knex('books')
-  .orderBy('title', 'asc')
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData));
-  });
-});
-
-router.get('/books/:id', (req, res, next) => {
-  let id = req.params.id;
-  knex('books')
-  .where('id', id)
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData[0]));
-  });
-});
-
-router.post('/books', (req, res, next) => {
-  knex('books')
-  .insert({
-    title: req.body.title,
-    author: req.body.author,
-    genre: req.body.genre,
-    description: req.body.description,
-    cover_url: req.body.coverUrl
-  }, '*')
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData[0]));
-  });
-});
-
-router.patch('/books/:id', (req, res, next) => {
-  let id = req.params.id;
-  knex('books')
-  .where('id', id)
-  .update(humps.decamelizeKeys(req.body))
-  .returning('*')
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData[0]));
-  });
-});
 
 router.delete('/books/:id', (req, res, next) => {
-  let id = req.params.id;
-  knex('books')
-  .where('id', id)
-  .returning(['title', 'author', 'genre', 'description', 'cover_url'])
-  .del()
-  .then(booksData => {
-    res.send(humps.camelizeKeys(booksData[0]));
-  });
+    let id = req.params.id;
+    if (isNaN(id)) {
+        return next(boom.create(404, 'Not Found'));
+    }
+    knex('books')
+        .where('id', id)
+        .returning(['title', 'author', 'genre', 'description', 'cover_url'])
+        .del()
+        .then(booksData => {
+            if (booksData[0]) {
+                res.send(humps.camelizeKeys(booksData[0]));
+            } else {
+                return next(boom.create(404, 'Not Found'));
+            }
+        });
 });
 
 module.exports = router;
